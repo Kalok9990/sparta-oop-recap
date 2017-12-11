dbe755d3c3c64bba434d28ed3b6e0a46368730ba
Tech-A29 <Tech-A29@Tech-A29s-MacBook-Air.local>
2017-12-11 12:18:58 +0000

    animal added

---
+++ livingOrganism/animal.rb
@@ -0,0 +1,37 @@
+require_relative "./living_organism"
+
+class Animal < LivingOrganism
+
+  def self.traits
+    puts 'Animals can breathe, eat, drink, speak and pro-create'
+  end
+
+  def breathe
+    puts "Animals can breathe"
+  end
+
+  def eat
+    puts "Animals can eat"
+  end
+
+  def drink
+    puts "Animals can drink"
+  end
+
+  def speak
+    puts "Animals can communicate"
+  end
+
+  def pro_create
+    puts "Animals can pro_create"
+  end
+end
+
+Animal.traits
+
+animal = Animal.new
+animal.breathe
+animal.eat
+animal.drink
+animal.speak
+animal.pro_create
---
+++ livingOrganism/living_organism.rb
@@ -0,0 +1,12 @@
+# global variable
+$global = 1
+
+class LivingOrganism
+
+  # class variable with the @@
+  @@number_of_living_organism = 0
+
+  def self.alive
+    puts 'Living organisms move, feed, respire (burn food to make energy), grow, sense their environment, excrete and reproduce. These seven characteristics are sometimes summarized by the words "MRS GREN". functions of: Movement, Respiration, Sensitivity, Growth, Reproduction,Excretion, Nutrition'
+  end
+end
