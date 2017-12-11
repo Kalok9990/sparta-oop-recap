fbd7a192a7ccf8de3ae885fb761589b6cea55289
Tech-A29 <Tech-A29@Tech-A29s-MacBook-Air.local>
2017-12-11 13:58:52 +0000

    new method for modules learnt

--- livingOrganism/mammals/dog.rb
+++ livingOrganism/mammals/dog.rb
@@ -1,2 +1,20 @@
 require_relative "../animal"
 require_relative "../animal_types/mammals"
+
+class Dog < Animal
+
+  include Mammals
+
+  def speak
+    puts "Woof woof"
+  end
+
+  def number_of_legs
+    Quadruped.legs
+  end
+end
+
+fido = Dog.new
+fido.speak
+fido.number_of_legs
+fido.breathe
